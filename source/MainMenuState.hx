1e332d07f4f9b6f60d2e403ef0879db219aeb5bb
Yugisgiy <hetgegeg@gmail.com>
2025-01-13 18:32:40 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -38,9 +38,8 @@ class MainMenuState extends MusicBeatState
 		#if MODS_ALLOWED 'mods', #end
 		//#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		//'credits',
-		//#if !switch 'donate', #end
+		'gallory'
 		'options'
-		'Gallery'
 	];
 
 	var magenta:FlxSprite;
