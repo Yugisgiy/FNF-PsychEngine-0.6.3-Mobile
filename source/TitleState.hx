8d6eb20f4ee7366c9b35e05367256f75ff3900b7
Yugisgiy <hetgegeg@gmail.com>
2025-03-07 20:04:54 +0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -507,7 +507,7 @@ class TitleState extends MusicBeatState
 					if (mustUpdate) {
 						MusicBeatState.switchState(new OutdatedState());
 					} else {
-						MusicBeatState.switchState(new MainMenuState());
+						MusicBeatState.switchState(new DeadAirMainMenuState());
 					}
 					closedState = true;
 				});
